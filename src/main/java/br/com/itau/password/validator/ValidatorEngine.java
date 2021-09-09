fabae87c255d4762f79f3fc0c3f9db067c8f61fc
fabiosouza <fabiosouza.web@gmail.com>
2021-09-09 20:49:56 -0300

    Testado config e ajuste no pom

--- src/main/java/br/com/itau/password/validator/ValidatorEngine.java
+++ src/main/java/br/com/itau/password/validator/ValidatorEngine.java
@@ -1,8 +1,12 @@
 package br.com.itau.password.validator;
 
+import java.util.List;
+
 import br.com.itau.password.model.PasswordResponse;
 
 public interface ValidatorEngine {
+	
+	public List<Validator> getValidators();
 	
 	public PasswordResponse validateAll(String password);
 }
--- src/main/java/br/com/itau/password/validator/impl/DefaultValidatorEngine.java
+++ src/main/java/br/com/itau/password/validator/impl/DefaultValidatorEngine.java
@@ -5,11 +5,13 @@ import java.util.List;
 import br.com.itau.password.model.PasswordResponse;
 import br.com.itau.password.validator.Validator;
 import br.com.itau.password.validator.ValidatorEngine;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class DefaultValidatorEngine implements ValidatorEngine {
 
+	@Getter
 	private List<Validator> validators;
 	
 	public DefaultValidatorEngine(List<Validator> validators) {
